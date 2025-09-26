be0b717d1a6b98f12da1197f29ca63e085a7966a
Taha Shaikh <97239186+NerdyTaha@users.noreply.github.com>
2025-09-26 19:21:25 +0530

    Setting DB w an ORM

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template 
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
+# inisde app object, set up key-value pairs in dictionary to use 
 
 #route for the home page
 @app.route("/")
@@ -18,8 +22,6 @@ def sign_in():
 @app.route("/register")
 def register():
     return render_template("register.html")
-
-
 
 # run the app only when executed stand-alone as a script and not when imported as a module
 if __name__ == "__main__":
