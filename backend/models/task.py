d2e626d2d1f0d888b03364dcde1f91895c1cae91
Taha Shaikh <97239186+NerdyTaha@users.noreply.github.com>
2025-09-29 18:23:49 +0530

    Added task_id, task_name and foreign key

--- backend/models/task.py
+++ backend/models/task.py
@@ -2,7 +2,12 @@ from backend.models import User
 from backend.app import db
 
 class Task(db.Model):
-
+    
     __tablename__ = "tasks"
-    user_id = db.Column()
-    task_set = db.Column()
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, foreign_key=User.user_id )
+    task_name = db.Column(db.String(300))
+
+    def __init__(self, user_id, task_name):
+        self.user_id = user_id
+        self.task_name = task_name
