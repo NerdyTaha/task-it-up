f50bc0c46a3ac9b23d6ee3b9beaa575181e758e9
Taha Shaikh <97239186+NerdyTaha@users.noreply.github.com>
2025-10-06 14:53:05 +0530

    user.py updated

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,10 +1,16 @@
 from backend.app import db, bcrypt
+from sqlalchemy.orm import relationship 
 
 class User(db.Model):
     __tablename__ = "users"
+    
+    #columns
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(150), nullable = False)
+    password = db.Column(db.String(70), nullable = False) # setting password length 60 because that is the length of bcrypt hashes
+    
+    #Relationship
+    tasks = relationship('Task', backref='user', lazy='true', cascade='all, delete-orphan')
 
     def __init__(self, email, password):
         self.email = email
