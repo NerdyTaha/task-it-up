f09fd43fc8aadb26624b06cd90127845c62c174f
Taha Shaikh <97239186+NerdyTaha@users.noreply.github.com>
2025-10-06 17:37:09 +0530

    major changes app.py

--- app.py
+++ app.py
@@ -3,40 +3,39 @@ from flask import Flask, render_template
 from backend.extensions import db, bcrypt, migrate
 from backend.models import user, task
 
+def create_app():
+    app = Flask(__name__) #instance or object of Flask- our App
 
-#instance or object of Flask- our App
-app = Flask(__name__)
+    # app object needs to have key-value pairs of env variables
+    app.config.from_mapping(
+        SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SECRET_KEY=os.getenv("SECRET_KEY")
+    )
 
-# inisde app object, set up key-value pairs in dictionary to use a databse
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#A key for maintaining sessions.
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY") 
+    # Initialize extensions.py with app
+    db.init_app(app)
+    bcrypt.init_app(app)
+    migrate.init_app(app, db)
 
-#instance or object of SQL_Alchemy - database 
-db = SQLAlchemy(app)
-#instance or object of Flask_Bcrypt to access encryption methods
-bcrypt = Bcrypt(app)
-# create Flask migrate instance 
-migrate = Migrate(app, db)
+    #Register all the routes
+    @app.route("/")
+    def home():
+        return render_template("sign-in.html")
 
-#route for the home page
-@app.route("/")
-def home():
-    return render_template("sign-in.html")
+    #setting the endpoint as 'sign-in' rather than the default function name
+    #url_for() in flask uses endpoint names and not URL's.
+    @app.route("/sign-in", endpoint="sign-in")
+    def sign_in():
+        return render_template("sign-in.html")
 
-#Route for sign-in page
-#setting the endpoint as 'sign-in' rather than the default function name
-#url_for() in flask uses endpoint names and not URL's.
-@app.route("/sign-in", endpoint="sign-in")
-def sign_in():
-    return render_template("sign-in.html")
+    @app.route("/register")
+    def register():
+        return render_template("register.html")
 
-#Route for Register page
-@app.route("/register")
-def register():
-    return render_template("register.html")
+    return app
 
 # run the app only when executed stand-alone as a script and not when imported as a module
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
