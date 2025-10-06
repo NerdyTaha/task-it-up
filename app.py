2685ce88dcb397f693929d02858cadbc787b5b3b
Taha Shaikh <97239186+NerdyTaha@users.noreply.github.com>
2025-10-06 16:56:52 +0530

    env var code change

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 
-"""
-from dotenv import load_dotenv
-from pathlib import Path
-
-# explicitly load .env from project root
-env_path = Path(__file__).resolve().parent.parent / ".env"
-load_dotenv(dotenv_path=env_path)
-""" 
-
 #instance or object of Flask- our App
 app = Flask(__name__)
 
